37c45918e7433cef8e18d1a87f74108582bfbbb7
Davy Leggieri <davy.leggieri@gmail.com>
2025-02-07 23:09:12 -0800

    Update prep-images.sh by adding Noble

--- prep-images.sh
+++ prep-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-UBUNTU_VERSIONS=${1:-"jammy kinetic lunar"}
+UBUNTU_VERSIONS=${1:-"noble jammy"}
 
 sudo apt-get update -qq < /dev/null > /dev/null
 sudo apt-get install -y -qq libguestfs-tools
