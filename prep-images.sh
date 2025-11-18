cdff29334fa612df6c317b7da69a6bff1cee3836
Davy Leggieri <davy.leggieri@gmail.com>
2025-11-17 20:26:08 -0800

    Updated to 24.04.3 and 25.10

--- prep-images.sh
+++ prep-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-UBUNTU_VERSIONS=${1:-"noble jammy"}
+UBUNTU_VERSIONS=${1:-"24.04.3 25.10"}
 
 sudo apt-get update -qq < /dev/null > /dev/null
 sudo apt-get install -y -qq libguestfs-tools
